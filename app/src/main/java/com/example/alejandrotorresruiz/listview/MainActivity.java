14de4306ccb0d84114091bc9507e404491bbde95
alejandrotorresruiz <Pollete456>
2017-11-12 15:04:13 +0100

    Añadido evento OnItemClickListener

--- app/src/main/java/com/example/alejandrotorresruiz/listview/MainActivity.java
+++ app/src/main/java/com/example/alejandrotorresruiz/listview/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.alejandrotorresruiz.listview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +14,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
-
+    private List<String > names;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView) ;
 
-        List<String > names = new ArrayList<String>();
+        names = new ArrayList<String>();
         names.add("Alejandro");
-        names.add("Alejandro");
-        names.add("Alejandro");
-        names.add("Alejandro");
+        names.add("Pepe");
+        names.add("Aroa");
+        names.add("Jessica");
 
         //Creamos el adaptador
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
 
         //Enlazamos el adaptador con nuestra vista.
         listView.setAdapter(adapter);
+
+
+        //Seleccionar elementos del textView
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Toast.makeText(MainActivity.this,"Has seleccionado "+names.get(position),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
